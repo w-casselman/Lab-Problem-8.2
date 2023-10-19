4db7be4bf36664a3729049b8150308039d8575b5
Wesley Casselman <w-casselman@onu.edu>
2023-10-19 11:01:50 -0400

    Completed countWords function

--- P8-2/P8-2.cpp
+++ P8-2/P8-2.cpp
@@ -1,4 +1,46 @@
 // P8-2
 // Wesley Casselman
 // October 19th, 2023
-// hello 
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int countWords(string str);
+
+int main()
+{
+	cout << countWords("Hey girl...");
+
+	return 0;
+}
+
+int countWords(string str)
+{
+	int words = 0;
+	
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (i == 0)
+		{
+			if (str[0] != ' ')
+			{
+				words++;
+			}
+		}
+		else if (i == (str.length() - 1))
+		{
+
+		}
+		else
+		{
+			if (str[i] == ' ' && str[i + 1] != ' ')
+			{
+				words++;
+			}
+		}
+	}
+
+	return words;
+}
